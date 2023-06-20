7414470e5023fba8555872b75eb134650f0c3cc1
willemNicolas <darwil.nicolas@gmail.com>
2023-06-20 14:58:29 +0100

    restructure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,14 @@
 //"use client";
-import Image from 'next/image'
 import styles from './page.module.css'
-import { Leva } from 'leva'
-import { Canvas } from '@react-three/fiber'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { World } from '../components/World'
-import { Main } from '../components/main'
+import { Main } from '../components/3D_components/Main'
 
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello World</h1>
       <Main />
-
-      <h1>End</h1>
     </main>
   )
 }
