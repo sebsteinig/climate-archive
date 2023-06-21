7e83d97bf393b576fdd97628d16d49b182abacb4
akina <renardakina@gmail.com>
2023-06-21 17:40:33 +0100

    2 routes for searcH

--- frontend/src/utils/api/api.tsx
+++ frontend/src/utils/api/api.tsx
@@ -1,33 +1,75 @@
 import axios from 'axios';
+import { log } from 'console';
+import {Publication, DefaultParameter} from "./types"
 
-const URL = "http://localhost:3000/"
+const URL_API = "http://localhost:3000/"
 const URL_IMAGE = "http://localhost:3060/"
 
-export async function search(path : string, query : Object) {
+
+/**
+ * @param query {'title', 'journal', 'authors_short'} 
+ * @returns experiments from publication as json
+ */
+export function searchPublication(query : Publication) {
+    if (query.title || query.journal || query.authors_short){
+        const query_param = Object.entries(query).map(bind => {
+            const [key,value] = bind
+            return `${key}=${JSON.stringify(value)}` 
+        }).join("&")
     
-    const query_param = Object.entries(query).map(bind => {
-        const [key,value] = bind
-        return `${key}=${JSON.stringify(value)}`
-    }).join("&")
+        let url = URL_API + "search/publication" + (query_param && "/?"+query_param)
+        return getData(url)
+    }
+}
 
-    let url = URL + "search/" + path + (query_param && "/?"+query_param)
 
+/**
+ * @param query for
+ * @returns labels
+ */
+export function searchLooking(query : {for:string}) {
+    const query_param = `for=${JSON.stringify(query.for)}`
+    let url = URL_API + "search/looking" + (query_param && "/?"+query_param)
+    return getData(url)    
+}
+
+/**
+ * @param query like ...
+ * @returns experiments with these characteristics
+ */
+export function search(query : DefaultParameter) {
+    if (!Object.values(query).every((value,index, number) => {value == null}) ){
+        let url = new URL(URL_API + "search/")
+        const search_param = new URLSearchParams(url.search)
+        Object.entries(query).map(bind => {
+            const [key,value] = bind
+            if (value) {
+                search_param.append(key, value.toString())  
+            }
+        })
+        
+        //let url = URL + "search/" + (query_param && "/?"+query_param)
+        return getData(url.toString())    
+    }
+}
+
+async function getData(url : string){
     try {
         let data = await axios.get(url)        
         return data.data
     } catch (error) {
-        throw error
+        console.log(error);
+        return {};
     }
 }
 
-export async function select(path : string, query : Object) {
-    
+export async function select(query : DefaultParameter) {
     const query_param = Object.entries(query).map(bind => {
         const [key,value] = bind
         return `${key}=${JSON.stringify(value)}`
     }).join("&")
 
-    let url = URL + "select/" + path + (query_param && "/?"+query_param)
+    let url = URL + "select/" + (query_param && "/?"+query_param)
     try {
         let data = await axios.get(url)        
         return data.data
---
+++ frontend/src/utils/api/types.tsx
@@ -0,0 +1,27 @@
+
+export type Publication = {
+    title : string
+    journal : string
+    authors_short : string
+    /*owner_name : string
+    owner_email : string
+    abstract : string
+    brief_desc : string
+    authors_full : string
+    year : number*/
+}
+
+
+export type DefaultParameter = {
+    like : string
+    with : string
+    config_name : string
+	extension : string
+	lossless : boolean
+	nan_value_encoding : number
+	threshold : number
+	chunks_time : number
+	chunks_vertical : number
+	rx : number
+	ry : number
+}
