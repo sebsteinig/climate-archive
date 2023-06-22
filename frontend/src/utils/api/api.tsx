2eae05e17f16909b27f3d17d9bb3241e217b62f6
akina <renardakina@gmail.com>
2023-06-22 11:29:23 +0100

    urls

--- frontend/src/utils/api/api.tsx
+++ frontend/src/utils/api/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { log } from 'console';
-import {Publication, DefaultParameter} from "./types"
+import {Publication, DefaultParameter, SelectCollectionParameter, SearchExperiment} from "./types"
 
 const URL_API = "http://localhost:3000/"
 const URL_IMAGE = "http://localhost:3060/"
@@ -11,14 +11,15 @@ const URL_IMAGE = "http://localhost:3060/"
  * @returns experiments from publication as json
  */
 export function searchPublication(query : Publication) {
+    let url = new URL("search/publication/", URL_API)
     if (query.title || query.journal || query.authors_short){
-        const query_param = Object.entries(query).map(bind => {
+        Object.entries(query).map(bind => {
             const [key,value] = bind
-            return `${key}=${JSON.stringify(value)}` 
-        }).join("&")
-    
-        let url = URL_API + "search/publication" + (query_param && "/?"+query_param)
-        return getData(url)
+            if (value) {
+                url.searchParams.append(key, JSON.stringify(value))  
+        }})
+        
+        return getData(url.href)
     }
 }
 
@@ -28,38 +29,47 @@ export function searchPublication(query : Publication) {
  * @returns labels
  */
 export function searchLooking(query : {for:string}) {
-    const query_param = `for=${JSON.stringify(query.for)}`
-    let url = URL_API + "search/looking" + (query_param && "/?"+query_param)
-    return getData(url)    
+    let url = new URL("search/looking/", URL_API)
+    url.searchParams.append("for", JSON.stringify(query.for))
+    return getData(url.href)    
 }
 
 /**
  * @param query like ...
  * @returns experiments with these characteristics
  */
-export function search(query : DefaultParameter) {
+export function search(query : SearchExperiment) {
     if (!Object.values(query).every((value,index, number) => {value == null}) ){
-        let url = new URL(URL_API + "search/")
-        const search_param = new URLSearchParams(url.search)
+        let url = new URL( "search/", URL_API)
         Object.entries(query).map(bind => {
             const [key,value] = bind
             if (value) {
-                search_param.append(key, value.toString())  
+                url.searchParams.append(key, JSON.stringify(value))  
             }
         })
-        
-        //let url = URL + "search/" + (query_param && "/?"+query_param)
-        return getData(url.toString())    
+        return getData(url.href)    
     }
 }
 
 async function getData(url : string){
     try {
-        let data = await axios.get(url)        
+        let data = await axios.get(url)
         return data.data
     } catch (error) {
         console.log(error);
         return {};
+    }
+}
+
+export async function selectCollection(query : SelectCollectionParameter) {
+    if (!Object.values(query).every((value,index, number) => {value == null})){
+        let url = new URL("select/collection/", URL_API)
+        Object.entries(query).map(bind => {
+            const [key,value] = bind
+            if (value) {
+                url.searchParams.append(key, JSON.stringify(value))  
+        }})
+        return getData(url.href)
     }
 }
 
