d5a1ecaf3ab4d0899dac78e59c826293fb24cef3
willemNicolas <darwil.nicolas@gmail.com>
2023-07-06 15:21:13 +0100

    css

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       {/* <Main /> */}
-      {/* <SearchBar/> */}
+      <SearchBar/>
       <TestImage />
     </main>
   )
