a25283094cbaa79eb9f54a612364b32549000634
willemNicolas <darwil.nicolas@gmail.com>
2023-07-10 09:55:08 +0100

    init variables

--- frontend/src/components/variables/Variables.tsx
+++ frontend/src/components/variables/Variables.tsx
@@ -1,3 +1,15 @@
+import { Clt } from "./supported/clt"
+import { Currents } from "./supported/currents"
+import { Height } from "./supported/height"
+import { Liconc } from "./supported/liconc"
+import { Mlotst } from "./supported/mlotst"
+import { Pfts } from "./supported/pfts"
+import { Pr } from "./supported/pr"
+import { Sic } from "./supported/sic"
+import { Snc } from "./supported/snc"
+import { Tas } from "./supported/tas"
+import { Tos } from "./supported/tos"
+import { Winds } from "./supported/winds"
 
 
 
@@ -8,7 +20,18 @@ type Props = {
 export function Variables({}:Props) {
     return (
         <>
-            
+            <Clt />
+            <Currents />
+            <Height />
+            <Liconc />
+            <Mlotst />
+            <Pfts />
+            <Pr />
+            <Sic />
+            <Snc />
+            <Tas />
+            <Tos />
+            <Winds />
         </>
     )    
 }
