8234917d0946323c9802ddda10feb1741c4313b8
sebsteinig <seb.steinig@gmail.com>
2023-09-14 16:23:24 +0100

    started using cameraControls

--- frontend/src/components/3D_components/Main.tsx
+++ frontend/src/components/3D_components/Main.tsx
@@ -4,15 +4,19 @@ import { Leva } from "leva"
 import React, { RefObject } from "react"
 import ReactDOM from "react-dom/client"
 import { World } from "./World"
+import { CameraControls } from '@react-three/drei';
 
 type Props = {
   tick: (delta: number) => void
 }
 
+const cameraControlRef = useRef<CameraControls | null>(null);
+
 export default function Main({ tick }: Props) {
   return (
     <div className="h-screen w-screen absolute top-0 left-0">
       <Leva collapsed={false} oneLineLabels={false} flat={true} />
+      {/* <CameraControls ref={cameraControlRef} /> */}
       <Canvas
         gl={{
           antialias: false,
@@ -21,7 +25,7 @@ export default function Main({ tick }: Props) {
           fov: 55,
           near: 0.1,
           far: 100,
-          position: [3, 2, 9],
+          position: [0, -7, 10],
         }}
       >
         <World tick={tick} />
